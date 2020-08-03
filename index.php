4ea5ca82a03bb47c420e28b917f0fd93803419db
Otavio Oshiro <otavioshiro@msn.com>
2020-08-03 18:29:26 -0400

    Adicionada a classe admin

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("vendor/autoload.php");
 //namespaces - pastas necessárias (que estão dentro do vendor) p/ o projeto funcionar
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 //trabalhar com rotas usa o Slim
 $app = new Slim();
@@ -16,6 +17,15 @@ $app->config('debug', true);
 $app->get('/', function(){
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+//rota p/ o funcionamento de ADmin
+$app->get('/admin', function(){
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
